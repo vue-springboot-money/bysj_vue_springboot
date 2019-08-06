3b399e55d5b93233311c82b9d59f8cc790b7a59b
Shao Zheng Bo <shaozhbo@cn.ibm.com>
2019-08-06 08:41:03 +0800

    添加构造函数+修改发布端口

--- server/src/main/java/com/example/pojo/ResultPojo.java
+++ server/src/main/java/com/example/pojo/ResultPojo.java
@@ -15,4 +15,14 @@ public class ResultPojo {
 	// 返回的对象
 	private Object object;
 
+	public ResultPojo() {
+		super();
+	}
+
+	public ResultPojo(int code, String msg, Object object) {
+		super();
+		this.code = code;
+		this.msg = msg;
+		this.object = object;
+	}
 }

3a4dd356e25c27a43df57c946c8b746b4bca92e0
Shao Zheng Bo <shaozhbo@cn.ibm.com>
2019-10-29 15:10:15 +0800

    上传天天食堂项目数据库设计

---
+++ server/db/天天食堂/tiantian_canteen.sql
@@ -0,0 +1,174 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tiantian_canteen
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_code`
+--
+
+DROP TABLE IF EXISTS `tb_code`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_code` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `no` varchar(100) NOT NULL COMMENT '订单编号',
+  `code` varchar(6) NOT NULL COMMENT '取餐码(六位纯数字)',
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态（0：未使用，1：已使用）',
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_code`
+--
+
+LOCK TABLES `tb_code` WRITE;
+/*!40000 ALTER TABLE `tb_code` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_code` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_menu`
+--
+
+DROP TABLE IF EXISTS `tb_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `name` varchar(100) NOT NULL COMMENT '菜单名',
+  `img` varchar(100) NOT NULL COMMENT '菜品图片路径',
+  `price` float NOT NULL DEFAULT '0' COMMENT '菜品价格',
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态（0：未上架，1：已上架）',
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_menu`
+--
+
+LOCK TABLES `tb_menu` WRITE;
+/*!40000 ALTER TABLE `tb_menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_news`
+--
+
+DROP TABLE IF EXISTS `tb_news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_news` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `title` varchar(100) NOT NULL COMMENT '新闻标题',
+  `img` varchar(100) DEFAULT NULL COMMENT '新闻配图路径',
+  `author` varchar(100) NOT NULL COMMENT '作者署名',
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态（0：未发布，1：已发布）',
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_news`
+--
+
+LOCK TABLES `tb_news` WRITE;
+/*!40000 ALTER TABLE `tb_news` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_order`
+--
+
+DROP TABLE IF EXISTS `tb_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `no` varchar(100) NOT NULL COMMENT '订单编号',
+  `uid` bigint(20) NOT NULL COMMENT '用户表主键',
+  `mid` bigint(20) NOT NULL COMMENT '菜品表主键',
+  `quantity` int(11) NOT NULL COMMENT '数量',
+  `price` float NOT NULL COMMENT '价格',
+  `comment` varchar(100) DEFAULT NULL COMMENT '评价（订单状态为1后可进行评价）',
+  `state` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态（0：进行中，1：已完成）',
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `tb_order_tb_user_fk` (`uid`),
+  KEY `tb_order_tb_menu_fk` (`mid`),
+  CONSTRAINT `tb_order_tb_menu_fk` FOREIGN KEY (`mid`) REFERENCES `tb_menu` (`id`),
+  CONSTRAINT `tb_order_tb_user_fk` FOREIGN KEY (`uid`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_order`
+--
+
+LOCK TABLES `tb_order` WRITE;
+/*!40000 ALTER TABLE `tb_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_user`
+--
+
+DROP TABLE IF EXISTS `tb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增主键',
+  `username` varchar(100) NOT NULL COMMENT '用户名（学号/工号）',
+  `passwrod` varchar(100) NOT NULL COMMENT '密码（MD5加密）',
+  `nickname` varchar(100) NOT NULL COMMENT '昵称(默认为用户名)',
+  `sex` tinyint(4) NOT NULL DEFAULT '1' COMMENT '性别（男: 1,女: 0）',
+  `type` tinyint(4) NOT NULL DEFAULT '1' COMMENT '类别（教师：0，学生：1）',
+  `balance` float NOT NULL DEFAULT '0' COMMENT '账户余额',
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+LOCK TABLES `tb_user` WRITE;
+/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'tiantian_canteen'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-29 15:09:14

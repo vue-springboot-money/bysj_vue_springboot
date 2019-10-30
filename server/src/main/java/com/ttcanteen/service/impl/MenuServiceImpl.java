226d830eb350ae0f2e9f23236060daa4757df471
Shao Zheng Bo <shaozhbo@cn.ibm.com>
2019-10-30 16:45:17 +0800

    上传文件上传接口

--- server/src/main/java/com/ttcanteen/service/impl/MenuServiceImpl.java
+++ server/src/main/java/com/ttcanteen/service/impl/MenuServiceImpl.java
@@ -13,7 +13,9 @@ import com.ttcanteen.service.MenuService;
 
 @Service
 public class MenuServiceImpl implements MenuService {
-	
+	@Value("${default.uploadPath}")
+	private String uploadPath;
+
 	@Value("${default.img}")
 	private String defaultImg;
 
@@ -25,22 +27,24 @@ public class MenuServiceImpl implements MenuService {
 	public TbMenuEntity createMenu(TbMenuEntity entity) {
 		// check是否有重名菜单
 		TbMenuEntity selectResult = tbMenuMapper.selectByName(entity.getName());
-		
+
 		// 存在重名菜单
 		if (selectResult != null) {
 			return null;
 		} else {
-			//不存在重名菜单，初始化数据
+			// 不存在重名菜单，初始化数据
 			// 设置状态为未上架状态
 			entity.setState((byte) 0);
 			// 如果未上传图片，赋予默认图片
 			if (entity.getImg() == null) {
 				entity.setImg(defaultImg);
+			} else {
+				entity.setImg(uploadPath + entity.getImg());
 			}
-			
+
 			// 添加菜单到数据库
 			int insertResult = tbMenuMapper.insert(entity);
-			
+
 			// 添加成功
 			if (insertResult == 1) {
 				return tbMenuMapper.selectByName(entity.getName());
@@ -61,28 +65,28 @@ public class MenuServiceImpl implements MenuService {
 	public List<TbMenuEntity> selectMenuListByPage(int pageNum) {
 		// 每页展示10条数据
 		int count = 10;
-		
+
 		if (pageNum < 1) {
 			pageNum = 1;
 		}
-		
+
 		List<TbMenuEntity> menuList = tbMenuMapper.selectByPage((pageNum - 1) * count, count);
 		return menuList;
 	}
 
 	@Override
 	public TbMenuEntity updateMenu(TbMenuEntity entity) {
-		
+
 		// 调整菜品状态为未上架状态
 		entity.setState((byte) 0);
-		
+
 		if (entity.getImg() == null || entity.getImg().equals("")) {
 			entity.setImg(defaultImg);
 		}
-		
+
 		// 更新数据库
 		int updateResult = tbMenuMapper.updateByPrimaryKey(entity);
-		
+
 		// 更新成功，返回更新后的结果
 		if (updateResult == 1) {
 			return tbMenuMapper.selectByPrimaryKey(entity.getId());
